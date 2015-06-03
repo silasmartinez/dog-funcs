1c197530cafd01142005aa05f62a730b73542906
silasmartinez <silasm@gmail.com>
2015-06-03 11:18:12 -0600

    Initial commit, dog funcs

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by silasmartinez on 6/3/15.
+ */
+var dog = require('./dog.js');
+
+var dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"];
+
+console.log(dog.howManyDogs(dogs));
+console.log(dog.nameLengths(dogs));
+console.log(dog.reverseDogNames(dogs));
+console.log(dog.firstThreeDogsWithEach(dogs));
